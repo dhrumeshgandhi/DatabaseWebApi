b0662f953334cd29f316e92c13f2910ddcd01a89
dhrumeshgandhi <dhrumeshgandhi@gmail.com>
2017-01-10 00:54:37 +0530

    first commit

---
+++ DatabaseApi/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseApi.Models;
+using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        User[] users = new User[]
+        {
+            new User {Id="obj1",Username="dcg",Password="dcg",FirstName="Dhrumesh",LastName="Gandhi" },
+            new User {Id="obj2",Username="sdp",Password="sdp",FirstName="Shubh",LastName="Patel" }
+        };
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+        public IHttpActionResult GetUser(string Id)
+        {
+            var user = users.FirstOrDefault((p) => p.Id == Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ DatabaseApi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseApi.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
